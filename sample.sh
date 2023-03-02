d0650fc9718f55f2cc798411ea22255929970ce7
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2023-03-02 12:43:18 -0500

    added a sample script for starter

---
+++ sample.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+
+function help(){
+    echo "Usage: sample.sh -e, -s, -d"
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -e|--extension)
+      EXTENSION="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--searchpath)
+      SEARCHPATH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -d|--default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+    -h|--help)
+      help
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+echo "FILE EXTENSION  = ${EXTENSION}"
+echo "SEARCH PATH     = ${SEARCHPATH}"
+echo "DEFAULT         = ${DEFAULT}"
