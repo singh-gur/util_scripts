641be029da5b4528348f283db6b7f5213f08dc74
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2023-03-02 12:56:24 -0500

    added tmp dir as option for go install

--- install_go.sh
+++ install_go.sh
@@ -3,15 +3,27 @@
 set -e
 
 VERSION=1.20.1
+TMP_DIR=/tmp
 
 function help(){
-    echo "Usage: install_go.sh -v/--version <version> (default: $VERSION)"
+    cat << help_eof 
+Usage: install_go.sh <options> 
+Options:
+    -v/--version <go version>           (default: $VERSION)
+    -t/--tmp_dir <temp dir location>    (default: $TMP_DIR)
+help_eof
+
 }
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     -v|--version)
       VERSION="$2"
+      shift
+      shift
+      ;;
+    -t|--tmp_dir)
+      TMP_DIR="$2"
       shift
       shift
       ;;
@@ -22,7 +34,7 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-pushd /tmp
+pushd $TMP_DIR
 
 wget https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
 
